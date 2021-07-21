87bb11f19459f72c61f29912b9aab0d37bf59e08
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-07-21 10:47:59 -0400

    Adds simple distribution parser

--- mltools/distribution/__init__.py
+++ mltools/distribution/__init__.py
@@ -3,3 +3,11 @@ from .normal import (
     Normal,
     MixedNormal
 )
+
+def parse_distribution(dist_type, dist_params):
+    dist_type = dist_type.lower()
+    if dist_type == "normal":
+        return Normal.instantiate(dist_params)
+    if dist_type == "mixed_normal":
+        return MixedNormal.instantiate(dist_params)
+    raise NotImplementedError(f"Distribution type \"{dist_type}\" not recognized")
