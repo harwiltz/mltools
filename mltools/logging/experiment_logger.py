cb5270ffa233ba0515995149bc0808104e72d18a
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-10-09 15:24:18 -0400

    Uses logging for CLIExperimentLogger

--- mltools/logging/experiment_logger.py
+++ mltools/logging/experiment_logger.py
@@ -1,3 +1,5 @@
+import logging
+
 class ExperimentLogger(object):
     def __init__(self, exp_name, tags=[], **kwargs):
         self.exp_name = exp_name
@@ -36,14 +38,16 @@ class ExperimentLogger(object):
         raise NotImplementedError
 
 class CLIExperimentLogger(ExperimentLogger):
-    def __init__(self, exp_name, **kwargs):
+    def __init__(self, exp_name, log_level=logging.INFO, **kwargs):
         super(CLIExperimentLogger, self).__init__(exp_name, **kwargs)
         self._has_warned_img = False
         self._has_warned_plt = False
-        print(f"Instantiated logger for experiment \"{exp_name}\"")
+        self._logger = logging.getLogger("Experiment Logger")
+        self._logger.setLevel(log_level)
+        self._logger.debug(f"Instantiated logger for experiment \"{exp_name}\"")
 
     def log_metric(self, tag, value, step, **kwargs):
-        print("[METRIC] {:>32} ({:>5}): {:>16.11f}".format(tag, step, value))
+        self._logger.info("[METRIC] {:>32} ({:>5}): {:>16.11f}".format(tag, step, value))
 
     def log_image(self, tag, img, step, **kwargs):
         if self._has_warned_img:
@@ -51,7 +55,7 @@ class CLIExperimentLogger(ExperimentLogger):
         else:
             warn_message_pre = f"[  WARN] {self.__class__} does not support image logging"
             warn_message_suf = f"skipping log of {tag} ({step})"
-            print(f"{warn_message_pre} -- {warn_message_suf}")
+            self._logger.warn(f"{warn_message_pre} -- {warn_message_suf}")
             self._has_warned_img = True
 
     def log_plt(self, tag, plt, step, **kwargs):
@@ -60,22 +64,22 @@ class CLIExperimentLogger(ExperimentLogger):
         else:
             warn_message_pre = f"[  WARN] {self.__class__} does not support plt logging"
             warn_message_suf = f"skipping log of {tag} ({step})"
-            print(f"{warn_message_pre} -- {warn_message_suf}")
+            self._logger.warn(f"{warn_message_pre} -- {warn_message_suf}")
             self._has_warned_plt = True
 
     def log_text(self, tag, text, **kwargs):
         tag = tag[:min(6, len(tag))]
-        print("[{:>6}] {}".format(tag, text))
+        self._logger.info("[{:>6}] {}".format(tag, text))
 
     def log_parameters(self, params, **kwargs):
-        print("<PARAMS>")
-        print(params)
-        print("</PARAMS>")
+        self._logger.info("<PARAMS>")
+        self._logger.info(params)
+        self._logger.info("</PARAMS>")
 
     def start_epoch(self, **kwargs):
         super(CLIExperimentLogger, self).start_epoch()
-        print(f"\n{64 * '='}\n")
-        print(f"Starting epoch {self.epoch + 1}...")
+        self._logger.debug(f"\n{64 * '='}\n")
+        self._logger.debug(f"Starting epoch {self.epoch + 1}...")
 
     def end_experiment(self):
-        print(f"\n[  DONE] Experiment \"{self.exp_name}\" has finished.")
+        self._logger.debug(f"\n[  DONE] Experiment \"{self.exp_name}\" has finished.")
